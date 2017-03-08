7494a894038ff272d34858430a103a58c48dadda
Ryan Perez <ryanp@Ryans-Air.hitronhub.home>
2017-03-08 16:11:06 -0700

    Added Floating Action Buttons and their animations

--- app/src/main/java/team21/pylonconstructor/MoodFeedActivity.java
+++ app/src/main/java/team21/pylonconstructor/MoodFeedActivity.java
@@ -1,13 +1,67 @@
 package team21.pylonconstructor;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MoodFeedActivity extends AppCompatActivity {
+    FloatingActionButton fab_plus, fab_updateMood, fab_search, fab_filter, fab_goToMap;
+    Animation FabOpen, FabClose, FabRotateClockwise, FabRotateCounterClockwise;
+    boolean isOpen = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_feed);
+
+        fab_plus = (FloatingActionButton) findViewById(R.id.fab_plus);
+        fab_updateMood = (FloatingActionButton) findViewById(R.id.fab_updateMood);
+        fab_search = (FloatingActionButton) findViewById(R.id.fab_search);
+        fab_filter = (FloatingActionButton) findViewById(R.id.fab_filter);
+        fab_goToMap = (FloatingActionButton) findViewById(R.id.fab_map);
+
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        FabRotateClockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        FabRotateCounterClockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_counterclockwise);
+
+        fab_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isOpen) {
+                    fab_goToMap.startAnimation(FabClose);
+                    fab_filter.startAnimation(FabClose);
+                    fab_search.startAnimation(FabClose);
+                    fab_updateMood.startAnimation(FabClose);
+                    fab_plus.startAnimation(FabRotateCounterClockwise);
+                    fab_goToMap.setClickable(false);
+                    fab_filter.setClickable(false);
+                    fab_search.setClickable(false);
+                    fab_updateMood.setClickable(false);
+                    isOpen = false;
+                }
+
+                else {
+                    fab_goToMap.startAnimation(FabOpen);
+                    fab_filter.startAnimation(FabOpen);
+                    fab_search.startAnimation(FabOpen);
+                    fab_updateMood.startAnimation(FabOpen);
+                    fab_plus.startAnimation(FabRotateClockwise);
+                    fab_goToMap.setClickable(true);
+                    fab_filter.setClickable(true);
+                    fab_search.setClickable(true);
+                    fab_updateMood.setClickable(true);
+                    isOpen = true;
+
+                }
+
+            }
+        });
+
     }
 }
