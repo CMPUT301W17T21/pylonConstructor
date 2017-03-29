c841566f5fff79b40335a997300a8e6cdf2e6f93
will1029 <william-qw@hotmail.com>
2017-03-28 18:03:16 -0600

    Camera Test

---
+++ app/src/androidTest/java/team21/pylonconstructor/CameraTest.java
@@ -0,0 +1,56 @@
+package team21.pylonconstructor;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.text.style.UpdateAppearance;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Willi_000 on 2017-03-28.
+ */
+
+public class CameraTest {
+    // IntentsTestRule is an extension of ActivityTestRule. IntentsTestRule sets up Espresso-Intents
+    // before each Test is executed to allow stubbing and validation of intents.
+    @Rule
+    public IntentsTestRule<UpdateMoodActivity> intentsRule = new IntentsTestRule<>(UpdateMoodActivity.class);
+
+    @Test
+    public void validateCameraScenario() {
+        // Create a bitmap we can use for our simulated camera image
+        Bitmap icon = BitmapFactory.decodeResource(
+                InstrumentationRegistry.getTargetContext().getResources(),
+                R.mipmap.ic_launcher_code);
+
+        // Build a result to return from the Camera app
+        Intent resultData = new Intent();
+        resultData.putExtra("data", icon);
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        // Stub out the Camera. When an intent is sent to the Camera, this tells Espresso to respond
+        // with the ActivityResult we just created
+        intending(toPackage("com.android.camera2")).respondWith(result);
+
+        // Now that we have the stub in place, click on the button in our app that launches into the Camera
+        onView(withId(R.id.take_photo_button)).perform(click());
+
+        // We can also validate that an intent resolving to the "camera" activity has been sent out by our app
+        intended(toPackage("com.android.camera2"));
+
+        // ... additional test steps and validation ...
+    }
+}
--- app/src/androidTest/java/team21/pylonconstructor/UpdateMoodTest.java
+++ app/src/androidTest/java/team21/pylonconstructor/UpdateMoodTest.java
@@ -1,9 +1,17 @@
 package team21.pylonconstructor;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.GeneralLocation;
 import android.support.test.espresso.action.Press;
 import android.support.test.espresso.action.Tap;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -16,6 +24,9 @@ import static android.support.test.espresso.action.ViewActions.actionWithAsserti
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
@@ -26,6 +37,11 @@ public class UpdateMoodTest {
 
     @Rule
     public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class);
+
+    //To test camera
+    //@Rule
+   // public IntentsTestRule<UpdateMoodActivity> intentsRule = new IntentsTestRule<>(UpdateMoodActivity.class);
+
 
     /**
      *  Custom click function for clicking invisible buttons
@@ -67,14 +83,6 @@ public class UpdateMoodTest {
         onView(withId(R.id.add_mood_event)).perform(click());
 
         //TODO Check output, and test other mood
-    }
-
-    //TODO Check photo (Take photo and cancel)
-    /**
-     * Take a photo test
-     */
-    public void photoTest() {
-
     }
 
     //TODO Check WithWho
