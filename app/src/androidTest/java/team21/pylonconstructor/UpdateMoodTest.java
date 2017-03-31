2c8341340fc8d5de5e46918184e0223d8a0bded7
will1029 <william-qw@hotmail.com>
2017-03-30 22:04:14 -0600

    Commit for minor change

--- app/src/androidTest/java/team21/pylonconstructor/UpdateMoodTest.java
+++ app/src/androidTest/java/team21/pylonconstructor/UpdateMoodTest.java
@@ -14,7 +14,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.PickerActions;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
