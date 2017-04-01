0d235056eabe4c5c7acdbeb3116998448bc20e4f
jroutledge <jroutled@ualberta.ca>
2017-03-31 21:56:59 -0600

    the moods now make different colored markers

--- app/src/main/java/team21/pylonconstructor/MapsActivity.java
+++ app/src/main/java/team21/pylonconstructor/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -47,15 +48,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         String key = getIntent().getStringExtra("key");
+        float emoji = BitmapDescriptorFactory.HUE_MAGENTA;
+
         ArrayList<Mood> moodArrayList = new ArrayList<>();
         if (key.equals("history")) {
             moodArrayList = Controller.getInstance().getAllMoods();
         } else if (key.equals("feed")) {
             moodArrayList = Controller.getInstance().getAllMoodsFeed();
         }
+        //TODO @jeff this should probably be in a try/catch block (also maybe check the colors)
         for (Mood mood : moodArrayList) {
+            switch (mood.getEmoji()) {
+                case ("HAPPY") : {
+                    emoji = BitmapDescriptorFactory.HUE_YELLOW;
+                    break;
+                }
+                case ("SAD") : {
+                    emoji = BitmapDescriptorFactory.HUE_BLUE;
+                    break;
+                }
+                case ("ANGRY") : {
+                    emoji = BitmapDescriptorFactory.HUE_RED;
+                    break;
+                }
+                case ("CONFUSED") : {
+                    emoji = BitmapDescriptorFactory.HUE_ROSE;
+                    break;
+                }
+                case ("DISGUSTED") : {
+                    emoji = BitmapDescriptorFactory.HUE_GREEN;
+                    break;
+                }
+                case ("SCARED") : {
+                    emoji = BitmapDescriptorFactory.HUE_VIOLET;
+                    break;
+                }
+                case ("SURPRISE") : {
+                    emoji = BitmapDescriptorFactory.HUE_ORANGE;
+                    break;
+                }
+                case ("SHAMEFUL") : {
+                    emoji = BitmapDescriptorFactory.HUE_CYAN;
+                    break;
+                }
+                    //surpriseButton, shamefulButton
+            }
             LatLng moodLatLng = new LatLng(mood.getLatitude(), mood.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(moodLatLng).title(mood.getTrigger()));
+            mMap.addMarker(new MarkerOptions()
+                    .position(moodLatLng)
+                    .title(mood.getTrigger())
+                    .icon(BitmapDescriptorFactory.defaultMarker(emoji)));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(moodLatLng));
         }
     }
