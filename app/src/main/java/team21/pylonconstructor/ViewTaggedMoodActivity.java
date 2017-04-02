fe5908a0e8bec26c7393da60b8589dc69bc080ea
Ryan Perez <ryanp@Ryans-Air.hitronhub.home>
2017-04-02 04:38:55 -0600

    added activity to view mood user was tagged in

---
+++ app/src/main/java/team21/pylonconstructor/ViewTaggedMoodActivity.java
@@ -0,0 +1,82 @@
+package team21.pylonconstructor;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.NotificationCompat;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+public class ViewTaggedMoodActivity extends AppCompatActivity {
+    private TaggedMoodAdapter adapter;
+    private List<Mood> moodList;
+    ElasticSearch elasticSearch;
+    Profile profile;
+    Context context = this;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_tagged_mood);
+
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("userinfo", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+
+
+        //TODO: replace these with notification query to populate moodList
+        elasticSearch = new ElasticSearch();
+        profile = Controller.getInstance().getProfile();
+        moodList = Controller.getInstance().getAllMoods();
+
+
+        adapter = new TaggedMoodAdapter(this, moodList);
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.prf_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+
+}
