72dc121f899dc207c7e80d842b34558ac15f3829
jroutledge <jroutled@ualberta.ca>
2017-04-02 23:01:08 -0600

    apparently there's another change

--- app/src/androidTest/java/team21/pylonconstructor/MapTest.java
+++ app/src/androidTest/java/team21/pylonconstructor/MapTest.java
@@ -1,9 +1,20 @@
 package team21.pylonconstructor;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static team21.pylonconstructor.TestHelper.customClick;
 
 /**
  * Created by Willi_000 on 2017-03-13.
@@ -11,14 +22,62 @@ import org.junit.runner.RunWith;
 
 /**
  * Tests map
+ *
+ * User Stories Tested: 06.01.01, 06.02.01, 06.03.01
+ * - Users can optionally add their locations to moods
+ * - Users can view moods from their mood histories on a map
+ * - Users can view moods from their mood feeds on a map
+ *
+ * @author jeffreyroutledge
  */
 @RunWith(AndroidJUnit4.class)
 public class MapTest {
+    TestHelper testHelper = new TestHelper();
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class);
+
     /**
-     * Test Map
+     * Test adding a mood with a location
+     */
+    @Test
+    public void checkLocation() {
+        //Login first
+        testHelper.logUserIn();
+
+        onView(withId(R.id.fab_plus)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab_plus)).perform(click());
+        onView(withId(R.id.fab_updateMood)).perform(customClick());
+        onView(withId(R.id.happy_button)).perform(click());
+        onView(withId(R.id.checkBox3)).check(matches(isNotChecked())).perform(scrollTo(), click());
+        onView(withId(R.id.add_mood_event)).perform(click());
+    }
+
+    /**
+     * Test Map from mood history
      */
     @Test
     public void checkMap() {
         //TODO Check Map
+        //Login first
+        testHelper.logUserIn();
+
+        onView(withId(R.id.fab_plus)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab_plus)).perform(click());
+        onView(withId(R.id.fab_map)).perform(customClick());
+    }
+
+    /**
+     * Test Map from mood feed
+     */
+    @Test
+    public void checkMapFromFeed() {
+        //Login first
+        testHelper.logUserIn();
+
+        onView(withId(R.id.mood_history_title_tview)).perform(click());
+        onView(withId(R.id.fab_plus)).check(matches(isDisplayed()));
+        onView(withId(R.id.fab_plus)).perform(click());
+        onView(withId(R.id.fab_map)).perform(customClick());
     }
 }
--- app/src/androidTest/java/team21/pylonconstructor/OfflineTest.java
+++ app/src/androidTest/java/team21/pylonconstructor/OfflineTest.java
@@ -4,5 +4,23 @@ package team21.pylonconstructor;
  * Created by jeffreyroutledge on 2017-04-02.
  */
 
+/**
+ * Tests offline mode
+ *
+ * User Stories Tested: 07.01.01
+ *
+ * - make sure online
+ * - add mood: offline edit
+ * - add mood: offline delete
+ * - go offline (turn on airplane mod)
+ * - add mood: offline add
+ * - delete mood: offline delete
+ * - edit mood: offline edit to say "offline editted:
+ * - go online
+ * - switch views back and forth
+ *
+ *
+ */
+
 public class OfflineTest {
 }
